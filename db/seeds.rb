95aea24403383417fb9fea397eabe69a26d9aa81
Nicolas BEURIOT <nbeuriot@gmail.com>
2022-08-24 15:40:08 +0200

    Rails-advanced Routing-Exo 2- Part-1

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,15 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+Restaurant.create({name: 'Casa Nova', address: 'Glacis', category: 'italian'})
+Restaurant.create({name: 'Nouveau Cigne', address: 'Ingeldorf', category: 'chinese'})
+Restaurant.create({name: 'Redstone', address: 'Ettelbruck', category: 'french'})
+Restaurant.create({name: 'Moulin de la Benne', address: 'Congnacais', category: 'french'})
+Restaurant.create({name: 'Massen', address: 'Weiswampach', category: 'french'})
